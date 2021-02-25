b1c1fd3040e9f339a4568f3d86b4a395cd468941
shwetha222 <79645969+shwetha222@users.noreply.github.com>
2021-02-25 22:13:54 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -77,7 +77,7 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://images.alphacoders.com/103/thumb-1920-1031089.jpg"
+SAITAMA_IMG = "https://raw.githubusercontent.com/shwetha222/GroupBot/main/Assests/repository-open-graph-template.png"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 Senku is hosted on one of Heroku's Servers and doesn't require any donations as of now but \
